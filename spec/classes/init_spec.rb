62f496386ae02968f9dee0d2be1fc49d86467b43
Chris <cpetersen@crystallized-software.com>
2019-10-15 15:46:14 -0500

    Fixing the default module name, since it changed in mid-stream.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-describe 'aix_vg_facts' do
+describe 'aix_lvm_facts' do
   context 'with default values for all parameters' do
-    it { should contain_class('aix_vg_facts') }
+    it { should contain_class('aix_lvm_facts') }
   end
 end
